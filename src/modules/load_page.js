c21608bf2887fb3c00ca14a26d31c29f5f37926e
P-Hsgw <contact@p-hsgw.com>
2021-04-05 17:51:18 +0900

    Add event listener to create a new Todo

--- src/modules/load_page.js
+++ src/modules/load_page.js
@@ -1,10 +1,10 @@
-
+import createTodo from "./todo"
 
 function eventListeners() {
   const firstTable = document.getElementById("tr-0")
   const modalBackground = document.getElementById("modal-background-0")
   const modalCloseButton = document.getElementById("modal-close-0")
-  const modalSubmitButton = document.getElementById("modal-submit")
+  const modalSubmitButton = document.getElementById("modal-submit-0")
 
   function toggleModal (e) {
     let modal = document.getElementById(`modal-${e.currentTarget.dataset.index}`)
@@ -23,10 +23,11 @@ function eventListeners() {
     toggleModal(e)
   })
 
-  modalSubmitButton.addEventListener("click", () => {
-    console.log('submitted')
-    // Add a logic that will create new todo after submit
+  modalSubmitButton.addEventListener("click", (e) => {
+    const title = document.getElementById(`title-${e.currentTarget.dataset.index}`).value
+    const description = document.getElementById(`description-${e.currentTarget.dataset.index}`).value
+
+    createTodo(title, description)
   })
 }
-
 export default eventListeners;
--- src/modules/todo.js
+++ src/modules/todo.js
@@ -1,5 +1,15 @@
+
+let todos = []
+
 const Todo = (title, description, dueDate, priority, notes) => {
   
   return {title, description, dueDate, priority, notes}
 }
 
+function createTodo(title, description, dueDate="10", priority="low", notes="none") {
+  const todo = Todo(title, description, dueDate , priority, notes)
+  todos.push(todo)
+  console.log(`Todo Array Test: ${todos}`)
+}
+
+export default createTodo
