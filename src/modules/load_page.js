02e375345402383ad4384bae9ddcbe83c85658b3
P-Hsgw <contact@p-hsgw.com>
2021-04-05 16:03:24 +0900

    Update event listeners, to make the code more universal

--- src/modules/load_page.js
+++ src/modules/load_page.js
@@ -1,27 +1,32 @@
 
 
 function eventListeners() {
-  const tr = document.getElementById("tr-0")
+  const firstTable = document.getElementById("tr-0")
   const modalBackground = document.getElementById("modal-background-0")
   const modalCloseButton = document.getElementById("modal-close-0")
+  const modalSubmitButton = document.getElementById("modal-submit")
 
-  function toggleModal () {
-    let modal = document.getElementById(`modal-${tr.dataset.index}`)
+  function toggleModal (e) {
+    let modal = document.getElementById(`modal-${e.currentTarget.dataset.index}`)
     modal.classList.toggle("is-active")
   }
 
-  tr.addEventListener("click", () => {
-    toggleModal()
+  firstTable.addEventListener("click", (e) => {
+    toggleModal(e)
   })
 
-  modalBackground.addEventListener("click", () => {
-    toggleModal()
+  modalBackground.addEventListener("click", (e) => {
+    toggleModal(e)
   })
 
-  modalCloseButton.addEventListener("click", () => {
-    toggleModal()
+  modalCloseButton.addEventListener("click", (e) => {
+    toggleModal(e)
   })
 
+  modalSubmitButton.addEventListener("click", () => {
+    console.log('submitted')
+    // Add a logic that will create new todo after submit
+  })
 }
 
 export default eventListeners;
